5a100b2cfc3bd29c2dc66b41aa30c37c7a1024b7
Jerry <as_nsygc@163.com>
2019-10-29 21:27:04 +0800

    换了电脑重新跑工程

--- orz/src/components/layout/Header.vue
+++ orz/src/components/layout/Header.vue
@@ -1,17 +1,16 @@
 <template>
 	<div class="header">
 		<router-link to="/" class="logo">LOGO</router-link>
-		<el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#242329" text-color="#fff" active-text-color="#ffd04b" router>
+		<el-menu :default-active="activeIndex" class="el_menu" mode="horizontal" @select="handleSelect" background-color="#242329" text-color="#fff" active-text-color="#ffd04b" router>
 			<el-menu-item index="/">Home</el-menu-item>
 			<el-menu-item index="/about">About</el-menu-item>
-			<el-submenu index="3">
-				<template slot="title">我的工作台</template>
-				<el-menu-item index="2-1">选项1</el-menu-item>
-				<el-menu-item index="2-2">选项2</el-menu-item>
-				<el-menu-item index="2-3">选项3</el-menu-item>
+			<el-submenu index="" class="user">
+				<template slot="title">pwx609952</template>
+				<el-menu-item index="/personalCenter">个人中心</el-menu-item>
+				<el-menu-item index="/myOrder">我的订单</el-menu-item>
+				<el-menu-item index="" @click="loginOut">注销</el-menu-item>
 			</el-submenu>
 		</el-menu>
-		<div class="user"></div>
 	</div>
 </template>
 
@@ -24,6 +23,11 @@ export default {
 		}
 	},
 	methods: {
+		// 注销
+		loginOut() {
+
+		},
+		// 选中的菜单
 		handleSelect(key, keyPath) {
 			console.log(key, keyPath)
 		}
